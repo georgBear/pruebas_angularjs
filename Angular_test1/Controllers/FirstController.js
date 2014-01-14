ea01786e6d0b9d946058f2f0eacd152803771ef9
Jorge Arce <jorge.arce@bear.com.mx>
2014-01-14 12:44:36 -0600

    With Modular Perspective - Jorge Arce.

--- Angular_test1/Controllers/FirstController.js
+++ Angular_test1/Controllers/FirstController.js
@@ -1,7 +1,15 @@
-﻿function SimpleController($scope) {
+﻿
+//Creating the module
+var demoApp = angular.module('demoApp', []);
+
+//Creating the controller inside of the just created demoApp module
+var controllers = {};
+controllers.SimpleController = function ($scope) {
     $scope.persons = [
-        {name:'John Smith',city:'Manhattan'}, 
-        {name:'John Doe',city:'Shanghai'}, 
-        {name:'Jane Doe',city:'Montreal'}
+        { name: 'John Smith', city: 'Manhattan' },
+        { name: 'John Doe', city: 'Shanghai' },
+        { name: 'Jane Doe', city: 'Montreal' }
     ];
-}
+};
+
+demoApp.controller(controllers);
