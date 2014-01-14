77be90ee5d02fa448a0723b17b8379d86dbfee77
Jorge Arce <jorge.arce@bear.com.mx>
2014-01-14 17:57:45 -0600

    Cambios para agregar ROutes al módulo, no funciona - Jorge Arce.

--- Angular_test1/Controllers/FirstController.js
+++ Angular_test1/Controllers/FirstController.js
@@ -1,7 +1,34 @@
 ﻿
 //Creating the module
-var demoApp = angular.module('demoApp', []);
+var demoApp = angular.module('demoApp', ['ngRoute']);
 
+//Configuring the demoApp module for adding custom routes in order to load partial views
+/*demoApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'FirstPage.htm'
+            })
+        .when('/partial1',
+            {
+                controller: 'SimpleController',
+                templateUrl: '../Partials/View1.html'
+            })
+        .when('/partial2',
+            {
+                controller: 'SimpleController',
+                templateUrl: '../Partials/View2.html'
+            })
+        .when('/sameLevel',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'sameLevel.html'
+            })
+        .otherwise({ redirectTo: '/' });
+
+});
+*/
 //Creating the controller inside of the just created demoApp module
 var controllers = {};
 controllers.SimpleController = function ($scope) {
@@ -10,6 +37,15 @@ controllers.SimpleController = function ($scope) {
         { name: 'John Doe', city: 'Shanghai' },
         { name: 'Jane Doe', city: 'Montreal' }
     ];
+    alert("Created SimpleController");
 };
 
 demoApp.controller(controllers);
+
+function render_view1() {
+    
+}
+
+function render_view2() {
+
+}
