cb5df06260337acbcd0fee133f749c6814c96f2c
Jorge Arce <jorge.arce@bear.com.mx>
2014-01-14 12:32:11 -0600

    Second step test - Jorge Arce

--- Angular_test1/Controllers/FirstController.js
+++ Angular_test1/Controllers/FirstController.js
@@ -1,1 +1,7 @@
-﻿
+﻿function SimpleController($scope) {
+    $scope.persons = [
+        {name:'John Smith',city:'Manhattan'}, 
+        {name:'John Doe',city:'Shanghai'}, 
+        {name:'Jane Doe',city:'Montreal'}
+    ];
+}
